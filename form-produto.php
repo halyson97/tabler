c8f9edb78fb60f211cef9e495a75977805adf8b6
Halyson <halyson97@gmail.com>
2021-03-07 18:48:05 -0300

    Form cadastrar produtos

--- form-produto.php
+++ form-produto.php
@@ -2,6 +2,16 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // The request is using the POST method
+
+    if(isset($_POST['descricao'], $_POST['estoque'], $_POST['codigoBarras'], $_POST['valor'])){
+        die('Cadastrar');
+    }
+}
+
+
+
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/form-produto.php';
 include __DIR__.'/includes/footer.php';
--- includes/form-produto.php
+++ includes/form-produto.php
@@ -1,49 +1,49 @@
 
-            <div class="row">              
-            <div class="col-lg-12">
-              <form class="card">
-                <div class="card-body">
-                  <h3 class="card-title">Novo produto</h3>
-                  <div class="row">
-		    <div class="col-md-12">
-                      <div class="form-group">
-			<label class="form-label">Descrição</label>
-			<input type="text" class="form-control" name="example-text-input" placeholder="Arroz..">
-		      </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                      <div class="form-group">
-                        <label class="form-label">Estoque</label>
-                        <input type="number" class="form-control" placeholder="10..">
-                      </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                      <div class="form-group">
-                        <label class="form-label">Código de barras</label>
-                        <input type="number" class="form-control" placeholder="78978978978978">
-                      </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                      <div class="form-group">
-                        <label class="form-label">Valor unitário</label>
-                        <div class="input-group">
-                          <span class="input-group-prepend">
-                            <span class="input-group-text">R$</span>
-                          </span>
-                          <input type="text" class="form-control text-right" aria-label="Valor">                         
-                        </div>
-                      </div>
-                    </div>                    
-                  </div>
-                </div>
-                <div class="card-footer text-left" style="display: flex; justify-content: space-between">
-		  <div>
-		    <a href="/produtos.php" class="btn btn-secondary">Voltar para produtos</a>
-		  </div>
-		  <div>
-		    <button type="submit" class="btn btn-primary">Confirmar</button>
-		  </div>                                    
-                </div>                
-              </form>
-            </div>
-          </div>
+<div class="row">              
+	<div class="col-lg-12">
+		<form class="card" method="post">
+			<div class="card-body">
+				<h3 class="card-title">Novo produto</h3>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<label class="form-label">Descrição</label>
+							<input type="text" class="form-control" name="descricao" placeholder="Arroz..">
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<div class="form-group">
+							<label class="form-label">Estoque</label>
+							<input type="number" class="form-control" placeholder="10.." name="estoque">
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<div class="form-group">
+							<label class="form-label">Código de barras</label>
+							<input type="number" class="form-control" placeholder="78978978978978" name="codigoBarras">
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-4">
+						<div class="form-group">
+							<label class="form-label">Valor unitário</label>
+							<div class="input-group">
+								<span class="input-group-prepend">
+									<span class="input-group-text">R$</span>
+								</span>
+								<input type="text" class="form-control text-right" aria-label="Valor" name="valor">                         
+							</div>
+						</div>
+					</div>                    
+				</div>
+			</div>
+			<div class="card-footer text-left" style="display: flex; justify-content: space-between">
+				<div>
+					<a href="/produtos.php" class="btn btn-secondary">Voltar para produtos</a>
+				</div>
+				<div>
+					<button type="submit" class="btn btn-primary">Confirmar</button>
+				</div>                                    
+			</div>                
+		</form>
+	</div>
+</div>
