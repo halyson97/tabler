fc0a6d0c43e4bf2687e2c3e1675eeb04e7300495
Halyson <halyson97@gmail.com>
2021-03-07 18:12:34 -0300

    Add composer

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+            </div>
+        </div>
+
+        <script src="./public/assets/js/require.min.js"></script>
+        <script>
+        requirejs.config({
+            baseUrl: './public'
+        });
+        </script>
+	
+        <script src="./public/assets/js/dashboard.js"></script>
+        <script src="./public/assets/plugins/charts-c3/plugin.js"></script>
+	
+        <script src="./public/assets/plugins/maps-google/plugin.js"></script>
+        <!-- Input Mask Plugin -->
+        <script src="./public/assets/plugins/input-mask/plugin.js"></script>
+    
+    </body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '02c4ed1f3b5bb7a973df6f1fd4fe9afad8605e60',
+    'name' => 'ac/tabler',
+  ),
+  'versions' => 
+  array (
+    'ac/tabler' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02c4ed1f3b5bb7a973df6f1fd4fe9afad8605e60',
+    ),
+  ),
+);
