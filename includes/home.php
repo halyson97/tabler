b2b3b9d171ca9e802037f24985df36446bff9be6
Halyson <halyson97@gmail.com>
2021-03-08 01:26:49 -0300

    Tela home redirecionando para produtos e vendas

--- includes/home.php
+++ includes/home.php
@@ -1,23 +1,27 @@
 <div class="page-header">
-    <h1 class="page-title">
-    Home
-    </h1>
+	<h1 class="page-title">
+	Home
+	</h1>
 </div>
 <div class="row row-cards">
-    <div class="col-6 col-sm-4 col-lg-2">
-    <div class="card">
-        <div class="card-body p-3 text-center">                    
-        <div class="h1 m-0"><?=$quantidadeProdutos?></div>
-        <div class="text-muted mb-4">Produtos</div>
-        </div>
-    </div>
-    </div>
+	<div class="col-6 col-sm-4 col-lg-2">
+	<div class="card">
+		<a href="produtos.php">
+			<div class="card-body p-3 text-center">                    
+			<div class="h1 m-0"><?=$quantidadeProdutos?></div>
+			<div class="text-muted mb-4">Produtos</div>
+		</a>
+	</div>
+	</div>
+	</div>
 <div class="col-6 col-sm-4 col-lg-2">
-    <div class="card">
-        <div class="card-body p-3 text-center">                    
-        <div class="h1 m-0"><?=$quantidadeVendas?></div>
-        <div class="text-muted mb-4">Venda</div>
-        </div>
-    </div>
-    </div>
+	<div class="card">
+		<a href="venda.php">
+			<div class="card-body p-3 text-center">                    
+			<div class="h1 m-0"><?=$quantidadeVendas?></div>
+			<div class="text-muted mb-4">Venda</div>
+		</a>
+	</div>
+	</div>
+	</div>
 </div>            
